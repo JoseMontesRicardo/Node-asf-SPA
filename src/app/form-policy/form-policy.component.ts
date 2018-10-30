6c4b5e29f0d7b8e06a9bcc79c4659d25fe70df8a
jose hilario <jmontes@resolvestudio.co>
2018-10-30 13:35:25 +0000

    Get data 'policy types' from API

--- src/app/form-policy/form-policy.component.ts
+++ src/app/form-policy/form-policy.component.ts
@@ -16,27 +16,15 @@ import { PolicyService } from '../policy.service';
 export class FormPolicyComponent implements OnInit {
 
   insurancePolicy: InsurancePolicy = new InsurancePolicy();
-  policies: PolicyType[] = [
-    {
-      "_id": "5bd7c02aa3bef840fa266f5a",
-      "name": "SOAT",
-      "value": 300000,
-      "insurancePolicys": []
-    },
-    {
-      "_id": "5bd7c03cb07f3d41258d9bd0",
-      "name": "Todo riesgo",
-      "value": 900000,
-      "insurancePolicys": []
-    }
-  ];
-  policyType: PolicyType = this.policies[0];
+  policies: PolicyType[];
+  policyType: PolicyType;
 
   constructor(public policyService: PolicyService) { }
 
   ngOnInit() {
     this.policyService.getList().then(data => {
-      console.log(data)
+      this.policies = data;
+      this.policyType = this.policies[0];
     });
   }
 
